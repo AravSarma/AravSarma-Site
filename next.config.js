833efcfae22c1503232fa553a6fb3fd1fff73cfa
AravSarma <46093473+AravSarma@users.noreply.github.com>
2026-01-04 02:57:24 -0800

    Make basePath opt-in for custom domain

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
-const repo = "AravSarma-Site";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd && isGitHubPages ? `/${repo}` : "",
-  assetPrefix: isProd && isGitHubPages ? `/${repo}/` : "",
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : "",
 };
 
 module.exports = nextConfig;
